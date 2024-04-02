018c5a42ace4c69c789cab8ab9531e9507074620
Damaride Putignano <damarideputignano@gmail.com>
2024-04-02 16:33:39 +0200

    Added Informazioni Component

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -2,6 +2,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProfileHeader from "./ProfileHeader";
 import Analisi from "./Analisi";
 import Risorse from "./Risorse";
+import Informazioni from "./Informazioni";
 
 const ProfilePage = function () {
     return (
@@ -10,6 +11,7 @@ const ProfilePage = function () {
             <Col className="col-12 col-md-8 col-lg-10 mt-4"><ProfileHeader /></Col>
             <Col className="col-12 col-md-8 col-lg-10 mt-4"><Analisi /></Col>
             <Col className="col-12 col-md-8 col-lg-10 mt-4"><Risorse /></Col>
+            <Col className="col-12 col-md-8 col-lg-10 mt-4"><Informazioni /></Col>
             </Row>
         </Container>
     )
