ff349eefa18b865f7a46c57180f021da5ee715e3
Damaride Putignano <damarideputignano@gmail.com>
2024-04-02 16:20:32 +0200

    Added Risorse

--- src/components/ProfilePage.jsx
+++ src/components/ProfilePage.jsx
@@ -1,6 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
 import ProfileHeader from "./ProfileHeader";
 import Analisi from "./Analisi";
+import Risorse from "./Risorse";
 
 const ProfilePage = function () {
     return (
@@ -8,6 +9,7 @@ const ProfilePage = function () {
             <Row className="d-flex flex-column">
             <Col className="col-12 col-md-8 col-lg-10 mt-4"><ProfileHeader /></Col>
             <Col className="col-12 col-md-8 col-lg-10 mt-4"><Analisi /></Col>
+            <Col className="col-12 col-md-8 col-lg-10 mt-4"><Risorse /></Col>
             </Row>
         </Container>
     )
