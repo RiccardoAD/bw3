7bf41a7116629f6568e0069c1796912ac2abe18a
Damaride Putignano <damarideputignano@gmail.com>
2024-04-03 16:26:54 +0200

    Finished modals on Experiences Page

--- src/components/ExperienceComponent.jsx
+++ src/components/ExperienceComponent.jsx
@@ -16,11 +16,17 @@ const ExperienceComponent = function () {
   const params = useParams();
   console.log("PARAMS", params);
 
-  /* STATO E FUNZIONI PER IL MODALE  */
+  /* STATO E FUNZIONI PER IL MODALE D'AGGIUNTA  */
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  /* STATO E FUNZIONI PER IL MODALE DI MODIFICA  */
+  const [appear, setAppear] = useState(false);
+
+  const handleDisappear = () => setAppear(false);
+  const handleAppear = () => setAppear(true);
 
   /* STATO E FUNZIONI PER IL FORM */
 
@@ -56,22 +62,23 @@ const ExperienceComponent = function () {
           <div id="otherCards">
             <Row>
               <Col>
+                {/*  PULSANTE "+" E MODALE D'AGGIUNTA CHE SI MOSTRA SOLO NELLA PAGINA DI MODIFICA */}
                 {params.userId && (
                   <>
                     <img
                       src={AddIcon}
                       className="editIcon"
-                      style={{ position: "absolute", top: "1rem", right: "4rem" }}
+                      style={{ position: "absolute", top: "1rem", right: "1.8rem" }}
                       onClick={handleShow}
                     ></img>
 
                     <Modal show={show} onHide={handleClose} size="lg">
                       <Modal.Header closeButton>
-                        <Modal.Title>Modifica Profilo</Modal.Title>
+                        <Modal.Title>Aggiungi Esperienza</Modal.Title>
                       </Modal.Header>
 
                       <Modal.Body>
-                        {/*   INIZIO FORM PER MODIFICARE I DATI DEL PROFILO */}
+                        {/*   INIZIO FORM PER AGGIUNGERE POSIZIONE LAVORATIVA */}
 
                         <Form ref={formRef} noValidate validated={validated} onSubmit={handleSubmit}>
                           <Form.Group className="mb-3" controlId="posizione lavorativa">
@@ -116,33 +123,85 @@ const ExperienceComponent = function () {
                     </Modal>
                   </>
                 )}
+                {/* FINE MODALE D'AGGIUNTA */}
 
-                {params.userId && (    <Link to="/profile/experiences">
-                  <img src={EditPen} className="editIcon" style={{ position: "absolute", top: "1rem" }} />
-                </Link>)}
+                {/* PULSANTE EDIT CON LINK ALLA PAGINA ESPERIENZE CHE SI MOSTRA SOLO NELLA PAGINA PROFILE */}
+                {!params.userId && (
+                  <Link to="/profile/experiences">
+                    <img src={EditPen} className="editIcon" style={{ position: "absolute", top: "1rem" }} />
+                  </Link>
+                )}
 
-            
+                {/* PULSANTE EDIT CHE ATTIVA MODALE CON FORM DI MODIFICA POSIZIONE LAVORATIVA CHE SI ATTIVA NELLA PAGINA EXPERIENCES */}
+
                 <h5 className="fw-bold pb-2">Esperienza</h5>
               </Col>
             </Row>
 
-            <Row className=" border-bottom py-3">
+            {/* ESPERIENZE STATICHE DA MAPPARE NON APPENA ABBIAMO LA FECTH: */}
+            <Row className=" border-bottom py-3" style={{ position: "relative" }}>
               <Col className="col-2 col-lg-1">
-                <img src={CompanyIcon} style={{ width: "50px" }}></img>
-              </Col>
-              <Col>
-                <p className="fw-bold">Mansione</p>
-                <p className="timeAndLocation">Periodo</p>
-                <p>
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, dolor alias quaerat, dolore quam amet
-                  dicta perspiciatis quas excepturi, repellendus facere quos? Incidunt sed minus vero optio laborum
-                  architecto eveniet.
-                </p>
-              </Col>
-            </Row>
+                {params.userId && (
+                  <>
+                    <img
+                      src={EditPen}
+                      className="editIcon"
+                      style={{ position: "absolute", top: "0.5rem", right: "1rem" }}
+                      onClick={handleAppear}
+                    />
 
-            <Row className="py-3">
-              <Col className="col-2 col-lg-1">
+                    <Modal show={appear} onHide={handleDisappear} size="lg">
+                      <Modal.Header closeButton>
+                        <Modal.Title>Modifica Esperienza</Modal.Title>
+                      </Modal.Header>
+
+                      <Modal.Body>
+                        {/*   INIZIO FORM PER MODIFICARE POSIZIONE LAVORATIVA */}
+
+                        <Form ref={formRef} noValidate validated={validated} onSubmit={handleSubmit}>
+                          <Form.Group className="mb-3" controlId="posizione lavorativa">
+                            <Form.Label>Posizione lavorativa</Form.Label>
+                            <Form.Control required type="text" placeholder="Scrivi qui la tua posizione lavorativa" />
+                          </Form.Group>
+                          <Form.Group className="mb-3" controlId="compagnia">
+                            <Form.Label>Compagnia</Form.Label>
+                            <Form.Control
+                              required
+                              type="text"
+                              placeholder="Scrivi qui la compagnia per cui hai lavorato"
+                            />
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="data di inizio">
+                            <Form.Label>Data di Inizio</Form.Label>
+                            <Form.Control required type="text" placeholder="Scrivi qui la tua data di inizio" />
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="data conclusione">
+                            <Form.Label>Data Conclusione</Form.Label>
+                            <Form.Control required type="text" placeholder="Scrivi qui la scadenza del tuo contratto" />
+                          </Form.Group>
+                          <Form.Group className="mb-3" controlId="area geografica">
+                            <Form.Label>Area Geografica</Form.Label>
+                            <Form.Control required type="text" placeholder="Scrivi qui la tua area geografica" />
+                          </Form.Group>
+                        </Form>
+
+                        {/*     FINE FORM
+                         */}
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={handleDisappear}>
+                          Close
+                        </Button>
+                        <Button type="submit" variant="primary" onClick={handleSubmit}>
+                          Save Changes
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+                  </>
+                )}
+
                 <img src={CompanyIcon} style={{ width: "50px" }}></img>
               </Col>
               <Col>
@@ -157,12 +216,14 @@ const ExperienceComponent = function () {
             </Row>
           </div>
 
-          <Row>
-            <Col className="col-12 sectionFooter">
-              Mostra tutte le esperienze
-              <img src={RightArrow} style={{ marginLeft: "10px" }}></img>
-            </Col>
-          </Row>
+          {!params.userId && (
+            <Row>
+              <Col className="col-12 sectionFooter">
+                Mostra tutte le esperienze
+                <img src={RightArrow} style={{ marginLeft: "10px" }}></img>
+              </Col>
+            </Row>
+          )}
         </Card>
       </Col>
     </Row>
