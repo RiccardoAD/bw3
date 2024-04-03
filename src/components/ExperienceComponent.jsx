73b8bd84af6d5c49bbf23dccb0f1cd661e6df88f
Damaride Putignano <damarideputignano@gmail.com>
2024-04-03 15:43:38 +0200

    More experience component

--- src/components/ExperienceComponent.jsx
+++ src/components/ExperienceComponent.jsx
@@ -6,9 +6,48 @@ import RightArrow from "../assets/svg/right_arrow_icon.svg";
 import CompanyIcon from "../assets/company_icon.png";
 import { Link, useParams } from "react-router-dom";
 
+import { useRef, useState } from "react";
+import Modal from "react-bootstrap/Modal";
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+
 const ExperienceComponent = function () {
+  /*  PARAMS CHE VERRANNO AGGIORNATI CON L'USERID DELLA FETCH */
   const params = useParams();
   console.log("PARAMS", params);
+
+  /* STATO E FUNZIONI PER IL MODALE  */
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  /* STATO E FUNZIONI PER IL FORM */
+
+  const [validated, setValidated] = useState(false);
+  const formRef = useRef(null);
+
+  const handleReset = () => {
+    formRef.current.reset();
+    setValidated(false);
+  };
+
+  const handleSubmit = (event) => {
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+
+    setValidated(true);
+
+    /*  Il reset dei campi funziona solo se non controllo la validità dei campi,
+      per questo per ora è commentato
+  
+     handleReset();
+     handleClose();
+    */
+  };
 
   return (
     <Row>
@@ -18,17 +57,71 @@ const ExperienceComponent = function () {
             <Row>
               <Col>
                 {params.userId && (
-                  <img
-                    src={AddIcon}
-                    className="editIcon"
-                    style={{ position: "absolute", top: "1rem", right: "4rem" }}
-                  ></img>
+                  <>
+                    <img
+                      src={AddIcon}
+                      className="editIcon"
+                      style={{ position: "absolute", top: "1rem", right: "4rem" }}
+                      onClick={handleShow}
+                    ></img>
+
+                    <Modal show={show} onHide={handleClose} size="lg">
+                      <Modal.Header closeButton>
+                        <Modal.Title>Modifica Profilo</Modal.Title>
+                      </Modal.Header>
+
+                      <Modal.Body>
+                        {/*   INIZIO FORM PER MODIFICARE I DATI DEL PROFILO */}
+
+                        <Form ref={formRef} noValidate validated={validated} onSubmit={handleSubmit}>
+                          <Form.Group className="mb-3" controlId="posizione lavorativa">
+                            <Form.Label>Posizione lavorativa</Form.Label>
+                            <Form.Control required type="text" placeholder="Scrivi qui la tua posizione lavorativa" />
+                          </Form.Group>
+                          <Form.Group className="mb-3" controlId="compagnia">
+                            <Form.Label>Compagnia</Form.Label>
+                            <Form.Control
+                              required
+                              type="text"
+                              placeholder="Scrivi qui la compagnia per cui hai lavorato"
+                            />
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="data di inizio">
+                            <Form.Label>Data di Inizio</Form.Label>
+                            <Form.Control required type="text" placeholder="Scrivi qui la tua data di inizio" />
+                          </Form.Group>
+
+                          <Form.Group className="mb-3" controlId="data conclusione">
+                            <Form.Label>Data Conclusione</Form.Label>
+                            <Form.Control required type="text" placeholder="Scrivi qui la scadenza del tuo contratto" />
+                          </Form.Group>
+                          <Form.Group className="mb-3" controlId="area geografica">
+                            <Form.Label>Area Geografica</Form.Label>
+                            <Form.Control required type="text" placeholder="Scrivi qui la tua area geografica" />
+                          </Form.Group>
+                        </Form>
+
+                        {/*     FINE FORM
+                         */}
+                      </Modal.Body>
+                      <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                          Close
+                        </Button>
+                        <Button type="submit" variant="primary" onClick={handleSubmit}>
+                          Save Changes
+                        </Button>
+                      </Modal.Footer>
+                    </Modal>
+                  </>
                 )}
 
-                <Link to="/profile/experiences">
-                  {" "}
-                  <img src={EditPen} className="editIcon" style={{ position: "absolute", top: "1rem" }} />{" "}
-                </Link>
+                {params.userId && (    <Link to="/profile/experiences">
+                  <img src={EditPen} className="editIcon" style={{ position: "absolute", top: "1rem" }} />
+                </Link>)}
+
+            
                 <h5 className="fw-bold pb-2">Esperienza</h5>
               </Col>
             </Row>
