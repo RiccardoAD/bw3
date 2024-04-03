b5ff8a9a70588681f341adfcf323db39e1b9eacd
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-04-03 15:30:19 +0200

    fetch profile

--- src/components/ProfileHeader.jsx
+++ src/components/ProfileHeader.jsx
@@ -1,40 +1,63 @@
-import { Container, Row, Col } from "react-bootstrap";
-import Card from 'react-bootstrap/Card';
-import EditPen from "../assets/svg/edit_pen_long.svg"
+import React, { useState, useEffect } from "react";
+import { Row, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
+import EditPen from "../assets/svg/edit_pen_long.svg";
 
 const ProfileHeader = function () {
-    return (
-       <Row>
+  const [profileData, setProfileData] = useState(null);
 
-       <Col>
-       <Card style={{ position: "relative"}}>
-      <Card.Img variant="top" style={{ height: '10rem' }} src="https://images.unsplash.com/photo-1707343843982-f8275f3994c5?q=80&w=1332&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" />
-      <div id="profilePicture">
-        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrptuijk8yZC1B_5W0kZGBl9GQHZ6YGNzeYROhsD83nQ&s"></img>
-      </div>
-      <Card.Body id="headerCard">
-        <img src={EditPen} className="editIcon"></img>
-    <h4 className="profileTitle">Nome Cognome</h4>
-        <Card.Text>
-         Sottotitolo per la professione
-        </Card.Text>
-        <p className="timeAndLocation">Località se dinamica • <a href="">Informazioni di contatto</a></p>
-      
-  
-        
-        <button className="mainBtn">Disponibile per</button>
-        <button className="secondaryBtn">Profilo</button>
-        <button className="tertiaryBtn">Altro</button>
-      </Card.Body>
-    </Card>
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const response = await fetch("https://striveschool-api.herokuapp.com/api/profile/me", {
+          headers: {
+            Authorization:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2NjBiYzlhYWEyODFkODAwMTlhM2VjNTciLCJpYXQiOjE3MTIwNDg1NTQsImV4cCI6MTcxMzI1ODE1NH0.6sHKqviDfFSd8qv2L8aNnu7plOcEuiqkhnhPbe72vKw",
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log("il mio profilo", data);
+        setProfileData(data);
+      } catch (error) {
+        console.error("Error fetching profile data:", error);
+      }
+    };
 
-       
-       </Col>
+    fetchProfileData();
+  }, []);
 
+  return (
+    <Row>
+      <Col>
+        <Card style={{ position: "relative" }}>
+          {profileData && (
+            <>
+              <Card.Img variant="top" style={{ height: "10rem" }} src={profileData.image} />
+              <div id="profilePicture">
+                <img src={profileData.image} alt="Profile" />
+              </div>
+              <Card.Body id="headerCard">
+                <img src={EditPen} className="editIcon" alt="Edit Pen Icon" />
+                <h4 className="profileTitle">
+                  {profileData.name} {profileData.surname}
+                </h4>
+                <Card.Text>{profileData.bio}</Card.Text>
+                <p className="timeAndLocation">
+                  {profileData.area} • <a href="#">Informazioni di contatto</a>
+                </p>
+                <button className="mainBtn">Disponibile per</button>
+                <button className="secondaryBtn">Profilo</button>
+                <button className="tertiaryBtn">Altro</button>
+              </Card.Body>
+            </>
+          )}
+        </Card>
+      </Col>
+    </Row>
+  );
+};
 
-       </Row>
-    )
-
-}
-
-export default ProfileHeader
+export default ProfileHeader;
