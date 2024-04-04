cc36b4d6db3d56882ff80ee555915d87cf15cad2
Damaride Putignano <damarideputignano@gmail.com>
2024-04-04 10:14:56 +0200

    Added userReducer

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,13 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import profileReducer from "../reducers/profileReducer";
 import isLoadingReducer from "../reducers/isLoading";
 import { postReducer } from "../reducers/post";
+import userReducer from "../reducers/userReducer";
 
 
 const store = configureStore({
   reducer: { 
     profile: profileReducer,
     isLoading: isLoadingReducer,
-    post: postReducer
+    post: postReducer,
+    user: userReducer,
  },
 });
 
