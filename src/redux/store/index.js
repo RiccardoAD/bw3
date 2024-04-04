e17261e56759dc1eefd2bf0ee2244183266fed89
Giovanni Maria Ricciolini <151955659+GianmariaRicciolini@users.noreply.github.com>
2024-04-04 10:44:00 +0200

    profileMeRedux

--- src/redux/store/index.js
+++ src/redux/store/index.js
@@ -2,14 +2,15 @@ import { configureStore } from "@reduxjs/toolkit";
 import profileReducer from "../reducers/profileReducer";
 import isLoadingReducer from "../reducers/isLoading";
 import { postReducer } from "../reducers/post";
-
+import meReducer from "../reducers/profileMeRed";
 
 const store = configureStore({
-  reducer: { 
+  reducer: {
     profile: profileReducer,
     isLoading: isLoadingReducer,
-    post: postReducer
- },
+    post: postReducer,
+    profileMeRed: meReducer,
+  },
 });
 
 export default store;
